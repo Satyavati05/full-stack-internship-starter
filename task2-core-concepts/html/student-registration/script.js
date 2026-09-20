26a0e99a7e882dfeadba8b2d66695e862b6608b8
Satyavati05 <satyayougni123@gmail.com>
2026-09-21 00:08:08 +0530

    Add form validation and DOM handling

--- task2-core-concepts/html/student-registration/script.js
+++ task2-core-concepts/html/student-registration/script.js
@@ -1,10 +1,66 @@
 const form = document.getElementById("registrationForm");
+const message = document.getElementById("message");
+
 form.addEventListener("submit", function(event) {
     event.preventDefault();
+    const fName = document.getElementById("fname").value.trim();
+    const lName = document.getElementById("lname").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const dob = document.getElementById("dob").value;
+    const gender = document.getElementById("gender").value;
+    const phone = document.getElementById("phone").value.trim();
+    const address = document.getElementById("address").value.trim();
+    const course = document.getElementById("course").value;
 
-    const fName = document.getElementById("fname").value;
+    if (fName.trim() === "") {
+        message.textContent = "Please enter your first name.";
+        return;
+    }
 
-    const message = document.getElementById("message");
+    if (lName.trim() === "") {
+        message.textContent = "Please enter your last name.";
+        return;
+    }
 
-    message.textContent = "Welcome, " + fName + "!";
+    if (email.trim() === "") {
+        message.textContent = "Please enter your email.";
+        return;
+    }
+    if (!email.includes("@") || !email.includes("gmail.com")) {
+        message.textContent = "Please enter a valid email address.";
+        return;
+    }
+
+
+    if (dob === "") {
+        message.textContent = "Please enter your date of birth.";
+        return;
+    }
+
+    if (gender === "") {
+        message.textContent = "Please enter your gender.";
+        return;
+    }
+
+
+    if (phone === "") {
+        message.textContent = "Please enter your phone number.";
+        return;
+    }
+    if (phone.length !== 10 || isNaN(phone)) {
+        message.textContent = "Phone number must be 10 digits.";
+        return;
+    }
+
+    if (address === "") {
+        message.textContent = "Please enter your address.";
+        return;
+    }
+
+    if (course === "" || course === "Select") {
+        message.textContent = "Please select a course.";
+        return;
+    }
+    message.style.color = "green"; //ALL CORRECT---SUCCESS MESSAGE
+    message.textContent = "Welcome, " + fName + "! Registration successful for the " + course + " course.";
 });
